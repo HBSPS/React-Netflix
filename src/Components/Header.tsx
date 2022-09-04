6f7bf33763576d29a1fc155d31594b01d95b1ce7
HBSPS <wiyeong79@naver.com>
2022-09-04 22:44:26 +0900

    :sparkles: Header
    Search 만들기 및 애니메이션 적용

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,7 @@
 import { Link, useMatch } from "react-router-dom";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Nav = styled.nav`
   display: flex;
@@ -51,7 +52,7 @@ const Item = styled.li`
   }
 `;
 
-const Circle = styled.span`
+const Circle = styled(motion.span)`
     position: absolute;
     width: 5px;
     height: 5px;
@@ -65,10 +66,26 @@ const Circle = styled.span`
 
 const Search = styled.span`
   color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
 
   svg {
     height: 25px;
   }
+`;
+
+const Input = styled(motion.input)`
+    transform-origin: right center;
+    position: absolute;
+    right: 0;
+    padding: 10px 10px;
+    padding-left: 40px;
+    background-color: transparent;
+    border: 1px solid ${(props) => props.theme.white.lighter};
+    color: white;
+    z-index: -1;
 `;
 
 const logoVariants = {
@@ -84,6 +101,9 @@ const logoVariants = {
 };
 
 function Header() {
+    const [searchOpen, setSearchOpen] = useState(false);
+    const toggleSearch = () => setSearchOpen((prev) => !prev);
+
     const homeMatch = useMatch("/");
     const tvMatch = useMatch("tv");
     return (
@@ -94,18 +114,19 @@ function Header() {
                 </Logo>
                 <Items>
                     <Link to="/">
-                        <Item>Home {homeMatch && <Circle />}</Item>
+                        <Item>Home {homeMatch && <Circle layoutId="circle"/>}</Item>
                     </Link>
                     <Link to="tv">
-                        <Item>TV Shows {tvMatch && <Circle />}</Item>
+                        <Item>TV Shows {tvMatch && <Circle layoutId="circle"/>}</Item>
                     </Link>
                 </Items>
             </Col>
             <Col>
                 <Search>
-                    <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <motion.svg onClick={toggleSearch} animate={{x: searchOpen ? -185 : 0}} transition={{type: "linear"}} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/motion.">
                         <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path>
-                    </svg>
+                    </motion.svg>
+                    <Input animate={{scaleX: searchOpen ? 1 : 0}} transition={{type: "linear"}} placeholder="Title, People. Genres" />
                 </Search>
             </Col>
         </Nav>
